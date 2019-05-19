66da61ccd82b2e806c1937185959423a9a5acc05
Richard Kang <richardkang1121@gmail.com>
2019-05-19 02:27:03 -0500

    progress on countNQueensSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -113,14 +113,14 @@ window.findNQueensSolution = function(n) {
     minArr = [];
     findSolution(0, col);
     if (numQueens !== n) {
-      var zeros = n - 1;
+      var firstRowWithoutPiece = n - 1;
       for (var z = 0; z < n; z++) {
         if (!board.rows()[z].includes(1)) {
-          zeros = z;
+          firstRowWithoutPiece = z;
           break;
         }
       }
-      for (var num = zeros; num >= 0; num--) {
+      for (var num = firstRowWithoutPiece; num >= 0; num--) {
         if (!board.rows()[num].includes(1)) {
           if (num === 0) {
             break;
@@ -145,7 +145,92 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n: n});
+  var numQueens, rowArr, colArr, majArr, minArr;
+  var majCalc = board._getFirstRowColumnIndexForMajorDiagonalOn
+  var minCalc = board._getFirstRowColumnIndexForMinorDiagonalOn
+  var addPiece = function (x, y, ma, mi) {
+    board.togglePiece(x, y);
+    numQueens++;
+    rowArr.push(x);
+    colArr.push(y);
+    majArr.push(ma);
+    minArr.push(mi);
+  };
+  var removePiece = function(x, y) {
+    board.togglePiece(x, y);
+    numQueens--;
+    rowArr.pop();
+    colArr.pop();
+    majArr.pop();
+    minArr.pop();
+  };
+
+  // recursive function passing in starting location
+  // if there is a piece at row col M&M that is passed in then skip
+  // dead space will be starting location row col major and minor diagonals
+  //each time we toggle a new piece it add new row col major and minor diagonals
+
+  var findSolutions = function(row, col) {
+    var maj = majCalc(row, col);
+    var min = minCalc(row, col);
+    if (!rowArr.includes(row) && !colArr.includes(col) && !majArr.includes(maj) && !minArr.includes(min)) {
+      if (!board._isInBounds(row, col)) {
+        return;
+      }
+      addPiece(row, col, maj, min);
+      findSolutions(row + 1, 0);
+    }
+    for (col; col < n; col++) {
+      maj = majCalc(row, col);
+      min = minCalc(row, col);
+      if (!rowArr.includes(row) && !colArr.includes(col) && !majArr.includes(maj) && !minArr.includes(min)) {
+        findSolutions(row, col);
+      }
+    }
+  };
+
+  //when we call recursive function using a for loop within a for loop to pass in different locations
+  //break out of for loop if we find a board where there are n queens
+
+  for (let col = 0; col < n; col++) {
+    // if (numQueens === n) {
+    //   solutionCount++;
+    // }
+    board = new Board({n: n});
+    numQueens = 0;
+    rowArr = [];
+    colArr = [];
+    majArr = [];
+    minArr = [];
+    findSolutions(0, col);
+    if (numQueens !== n) {
+      var firstRowWithoutPiece = n - 1;
+      for (var z = 0; z < n; z++) {
+        if (!board.rows()[z].includes(1)) {
+          firstRowWithoutPiece = z;
+          break;
+        }
+      }
+      for (var num = firstRowWithoutPiece; num >= 0; num--) {
+        if (!board.rows()[num].includes(1)) {
+          if (num === 0) {
+            break;
+          }
+          var prevPiece = board.rows()[num - 1].indexOf(1);
+          removePiece(num - 1, prevPiece);
+          findSolutions(num - 1, prevPiece + 1);
+          if (numQueens === n) {
+            solutionCount++;
+          }
+        }
+      }
+    }
+    if (numQueens === n) {
+      solutionCount++;
+    }
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
